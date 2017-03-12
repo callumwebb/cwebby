08144822af9926121034d3af512f2281fcce0f2d
callum webb <cwebb235@gmail.com>
2017-03-12 19:09:29 +0800

    progress on force layout

--- static/js/berries.js
+++ static/js/berries.js
@@ -1,8 +1,8 @@
 
 
-var  margin = {top: 10, right: 20, bottom: 10, left: 20},
-     width = 25,
-     height = 25;
+var  margin = {top: 80, right: 40, bottom: 40, left: 80},
+     width = 70,
+     height = 70;
 
 var svg = d3.select("figure.berryLabels").append("svg")
   .attr("viewBox", 0 + " " + 0 + " " + (width + margin.left + margin.right) + " " + (height + margin.bottom + margin.top))
@@ -10,6 +10,63 @@ var svg = d3.select("figure.berryLabels").append("svg")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+
+// Predicted labels
+svg.append("text")
+  .attr("transform", "translate(" + (width / 2) + " ," + (- 2.5 * margin.top / 3) + ")")
+  .style("text-anchor", "middle")
+  .text("predicted");
+svg.append("text")
+  .attr("transform", "translate(0," + (-1.1 * margin.top / 2) + ")")
+  .style("text-anchor", "middle")
+  .style("alignment-baseline", "middle")
+  .style("font-weight", "bold")
+  .text("+");
+svg.append("text")
+  .attr("transform", "translate(" + width + "," + (-1.1 * margin.top / 2) + ")")
+  .style("text-anchor", "middle")
+  .style("alignment-baseline", "middle")
+  .style("font-weight", "bold")
+  .text("−");
+
+// Actual labels
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", - 2.5 * margin.left / 3)
+  .attr("x", - height / 2)
+  .style("text-anchor", "middle")
+  .text("actual");
+svg.append("text")
+  .attr("x", 0 - 1.1 * margin.left / 2)
+  .attr("y", 0)
+  .style("text-anchor", "middle")
+  .style("alignment-baseline", "middle")
+  .style("font-weight", "bold")
+  .text("+");
+svg.append("text")
+  .attr("x", 0 - 1.1 * margin.left / 2)
+  .attr("y", height)
+  .style("text-anchor", "middle")
+  .style("alignment-baseline", "middle")
+  .style("font-weight", "bold")
+  .text("−");
+
+// svg.selectAll("text")
+//   .data([1, 1, 0, 0]) // 1s for '+'
+//   .enter().append("text")
+//   .attr("transform", function(d, i) {
+//     return("translate(")
+//   })
+
+// svg.append("text")
+//   .attr("transform", "translate(")
+// svg.append("text")
+//   .attr("transform", "rotate(-90)")
+//   .attr("y", -40)
+//   .attr("x", 0 - (height / 2))
+//   .style("text-anchor", "middle")
+//   .text("true positive rate");
+
 
 var berryGroup = svg.selectAll("g")
   .data([1, 1, 0, 0]) // 1s for raspberries
@@ -19,43 +76,9 @@ var berryGroup = svg.selectAll("g")
     })
   
 berryGroup.append("circle") // the berry
-    .attr("r", 5)
-    .attr("r", 5)
+    .attr("r", 15)
     .attr("class", function(d) {return (d == 1 ? "raspberry" : "blueberry")});
 
 berryGroup.append("circle") // the label (perimeter circle)
-    .attr("r", 8)
+    .attr("r", 22)
     .attr("class", function(d, i) {return ((i % 2) == 0 ? "pos" : "neg")});
-
-// svg.selectAll("circle")
-//   .data([1, 1, 0, 0])
-//   .enter().append("circle")
-//   .attr("cy", function(d) {return (1 - d) * height;})
-//   .attr("r", 5)
-//   .attr("cx", function(d, i) {return (i % 2) * width;})
-//   .attr("r", 5)
-//   .attr("fill", function(d) {return (d == 1 ? "orange" : "blue")});
-
-// var berry = svg.selectAll("circle")
-//   .data([1, 1, 0, 0]);
-
-// var berryEnter = svg.selectAll("circle").append("circle")
-//   .data([1, 1, 0, 0])
-//   .attr("cx", function(d, i) {return (i % 2) * 10;})
-//   .attr("r", 10);
-
-
-// svg.append("circle")
-//   .attr("class", "raspberry")
-//   .attr("fill", "red")
-//   .attr("cx", width / 4)
-//   .attr("cy", height / 2)    
-//   .attr("r", 5)
-
-
-// svg.append("circle")
-//   .attr("class", "blueberry")
-//   .attr("fill", "blue")
-//   .attr("cx", 3 * width / 4)
-//   .attr("cy", height / 2)    
-//   .attr("r", 5)
